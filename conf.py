3871941a396504d50d0d90e9419f097e0d376d4a
wojtekczajka <w.czajka@gmail.com>
2023-06-14 02:52:15 +0200

    deploy release :)

--- conf.py
+++ conf.py
@@ -1,3 +1,4 @@
-frontend_address = "http://127.0.0.1:8080"
-backend_address = "http://127.0.0.1:8000"
-# backend_address = "https://db-browser-backend.onrender.com"
+frontend_address = "https://wojtekczajka.github.io/db_browser_front"
+backend_address = "https://db-browser-backend.onrender.com"
+# frontend_address = "http://127.0.0.1:8080"
+# backend_address = "http://127.0.0.1:8000"
--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine, inspect
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:!Bioshock13!@database-1.cqyfd9wssou1.eu-north-1.rds.amazonaws.com:3306/pki_browser_db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:!Bioshock13!@database-1.cqyfd9wssou1.eu-north-1.rds.amazonaws.com:3306/pki_browser_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 insp = inspect(engine)
